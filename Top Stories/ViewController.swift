5c48ee2c5f36f87868c50ba4fe7e78980be75f1d
Sydney Ripsky <sripsky@bsd220.org>
2020-01-28 10:42:56 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Sydney Ripsky on 1/28/20.
+//  Copyright © 2020 Sydney Ripsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
