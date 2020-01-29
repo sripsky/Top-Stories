58dfd73f5a7b420d97b5e507b3490231e21226fb
Sydney Ripsky <sripsky@bsd220.org>
2020-01-29 10:48:00 -0600

    Finished initial set up of sources table view

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -17,16 +17,16 @@ class SourcesViewController: UITableViewController {
         super.viewDidLoad()
         self.title = "News Sources"
         let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
-  if let url = URL(string: query) {
-     if let data = try? Data(contentsOf: url) {
-        let json = try! JSON(data: data)
-        if json["status"] == "ok" {
-           parse(json: json)
-           return
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["status"] == "ok" {
+                    parse(json: json)
+                    return
+                }
+            }
         }
-     }
-  }
-  loadError()
+        loadError()
     }
     
     func parse (json : JSON)
@@ -42,10 +42,22 @@ class SourcesViewController: UITableViewController {
     }
     
     func loadError() {
-         let alert = UIAlertController(title: "Loading Error",
-                                     message: "There was a problem loading the news feed",
-                              preferredStyle: .actionSheet)
-         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-         present(alert, animated: true, completion: nil) }
+        let alert = UIAlertController(title: "Loading Error",
+                                      message: "There was a problem loading the news feed",
+                                      preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil) }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sources.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = sources[indexPath.row]
+        cell.textLabel?.text = source["name"]
+        cell.detailTextLabel?.text = source["description"]
+        return cell
+    }
 }
 
