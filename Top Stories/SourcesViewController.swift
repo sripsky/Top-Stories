d2c3ca3aa17792c75e6d014ea986bb86a33f0c5c
Sydney Ripsky <sripsky@bsd220.org>
2020-01-31 10:29:02 -0600

    Finished background data loading routine

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -17,16 +17,20 @@ class SourcesViewController: UITableViewController {
         super.viewDidLoad()
         self.title = "News Sources"
         let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
-        if let url = URL(string: query) {
-            if let data = try? Data(contentsOf: url) {
-                let json = try! JSON(data: data)
-                if json["status"] == "ok" {
-                    parse(json: json)
-                    return
+        DispatchQueue.global(qos: .userInitiated).async {
+            [unowned self] in
+            
+            if let url = URL(string: query) {
+                if let data = try? Data(contentsOf: url) {
+                    let json = try! JSON(data: data)
+                    if json["status"] == "ok" {
+                        self.parse(json: json)
+                        return
+                    }
                 }
             }
+            self.loadError()
         }
-        loadError()
     }
     
     func parse (json : JSON)
@@ -38,15 +42,23 @@ class SourcesViewController: UITableViewController {
             let source = ["id" : id, "name" : name, "description" : description]
             sources.append(source)
         }
-        tableView.reloadData()
+        DispatchQueue.main.async {
+            [unowned self] in
+            self.tableView.reloadData()
+            
+        }
     }
     
     func loadError() {
-        let alert = UIAlertController(title: "Loading Error",
-                                      message: "There was a problem loading the news feed",
-                                      preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        present(alert, animated: true, completion: nil) }
+        DispatchQueue.main.async {
+            [unowned self] in
+            let alert = UIAlertController(title: "Loading Error",
+                                          message: "There was a problem loading the news feed",
+                                          preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return sources.count
